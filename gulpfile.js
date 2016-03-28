e354c0ce1cc5287cb3e27ee3c759963c9814f723
Sandra Szenti <alexandraszenti@gmail.com>
2016-03-28 22:51:13 +0100

    optimising css and CRP

--- gulpfile.js
+++ gulpfile.js
@@ -8,26 +8,35 @@ var babel = require('gulp-babel');
 var concat = require('gulp-concat');
 var sourcemaps = require('gulp-sourcemaps');
 var minify = require('gulp-minify');
+var critical = require('critical');
+var cleanCSS = require('gulp-clean-css');
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass', 'js'], function() {
 
     browserSync.init({
-        server: "./app"
+        server: "./src"
     });
 
     gulp.watch("src/img/*", ['images']);
-    gulp.watch("src/scss/*.scss", ['sass']);
+    gulp.watch("src/scss/*.scss", ['minify-css']);
     gulp.watch("src/js/*.js", ['js']);
-    gulp.watch("app/*.html").on('change', browserSync.reload);
+    gulp.watch("src/*.html", ['critical']);
+    gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
     return gulp.src("src/scss/*.scss")
         .pipe(sass())
-        .pipe(gulp.dest("app/css"))
-        .pipe(browserSync.stream());
+        .pipe(gulp.dest("src/css"));
+});
+
+gulp.task('minify-css', ['sass'], function() {
+  return gulp.src('src/css/*.css')
+    .pipe(cleanCSS({compatibility: 'ie10'}))
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.stream());
 });
 
 // JS minify and concat
@@ -46,6 +55,18 @@ gulp.task('images', function() {
 			use: [pngquant()]
 		}))
 		.pipe(gulp.dest('app/img'));
+});
+
+gulp.task('critical', ['sass'], function (cb) {
+    critical.generate({
+        inline: true,
+        base: 'src/',
+        src: 'index.html',
+        dest: 'app/index.html',
+        minify: true,
+        width: 480,
+        height: 720
+    });
 });
 
 gulp.task('default', ['serve']);
